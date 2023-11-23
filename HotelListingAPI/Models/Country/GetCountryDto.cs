dfc388434ff6ebf8f461f4c7b5ecf446b7fbba08
irondead <miguel.moralesa@hotmail.com>
2023-11-22 22:51:17 -0600

    REST API Development Best Practices Automapper and Repositories

---
+++ HotelListingAPI/Models/Country/GetCountryDto.cs
@@ -0,0 +1,10 @@
+﻿using HotelListingAPI.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListingAPI.Models.Country
+{
+    public class GetCountryDto : BaseCountryDto
+    {
+        public int Id { get; set; }
+    }
+}
--- HotelListingAPI/Program.cs
+++ HotelListingAPI/Program.cs
@@ -1,4 +1,7 @@
+using HotelListingAPI.Configurations;
+using HotelListingAPI.Contracts;
 using HotelListingAPI.Data;
+using HotelListingAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -24,7 +27,13 @@ builder.Services.AddCors(options =>
     .AllowAnyMethod());
 });
 
-builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration                                                                                                                                                                         ));
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
+
 
 var app = builder.Build();
 
